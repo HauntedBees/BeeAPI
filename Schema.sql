c83657b9e52e8370a9208c9eb178b928d29462b7
Haunted Bees <fench@hauntedbees.com>
2021-01-15 23:45:26 -0800

    sure

--- Schema.sql
+++ Schema.sql
@@ -1,25 +1,28 @@
-CREATE DATABASE IF NOT EXISTS `beeaccount`
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+CREATE DATABASE IF NOT EXISTS `beeaccount` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `beeaccount`;
 
-CREATE TABLE `users` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `username` NVARCHAR(45) NOT NULL,
-  `password` VARCHAR(255) DEFAULT NULL,
-  `rolebits` INT DEFAULT NULL,
-  `source` VARCHAR(45) DEFAULT NULL,
-  `accesstoken` VARCHAR(100) DEFAULT NULL,
-  `accesstokensecret` VARCHAR(100) DEFAULT NULL,
-  `externalname` NVARCHAR(100) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `apitoken` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `type` varchar(15) NOT NULL,
+  `token` varchar(100) NOT NULL,
+  `secret` varchar(100) NOT NULL,
+  `created` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-);
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `apitoken` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR(15) NOT NULL,
-  `token` VARCHAR(100) NOT NULL,
-  `secret` VARCHAR(100) NOT NULL,
-  `created` DATETIME NOT NULL,
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `rolebits` int(11) DEFAULT NULL,
+  `source` varchar(45) DEFAULT NULL,
+  `accesstoken` varchar(100) DEFAULT NULL,
+  `accesstokensecret` varchar(100) DEFAULT NULL,
+  `externalname` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-);
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
