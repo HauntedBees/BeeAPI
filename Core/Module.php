72cf1ebee2eb8d1588b129d2c7b47f4bb87c1a58
HauntedBees <fench@hauntedbees.com>
2021-11-25 14:39:06 -0800

    it's the bee cache

--- Core/Module.php
+++ Core/Module.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 require_once "Models.php";
 require_once "BeeAuth.php";
+require_once "BeeCache.php";
 require_once "BeeController.php";
 require_once "BeeSecureController.php";
 require_once "BeeDB.php";
